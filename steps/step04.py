d045f65204e53345d1d5704de4dda22bc486ce2b
rain0430 <j2hoon85@naver.com>
2021-02-22 12:44:17 +0900

    1st msg

---
+++ steps/step04.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+class Square(Function):
+    def forward(self, x):
+        return x  ** 2
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+def numerical_diff(f, x, eps = 1e-4):
+    """수치 미분 함수 구현
+    Args:
+        f : Square Class Parameter
+        X (int): input data
+        eps: 수학에서 작은 양의 값을 나타냄
+    
+    Returns:
+        수치미분 공식을 활용한 값
+    """
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+
+    return (y1.data - y0.data) / (2 * eps)
+
+f = Square()
+x = Variable(np.array(2.0))
+dy = numerical_diff(f, x)
+print(dy)
