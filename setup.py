0f28dfa9b6893ad0200446a02f1a68f668e25112
dschloe <j2hoon85@gmail.com>
2021-03-03 17:00:28 +0900

    updated

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import os
 import io
 from setuptools import find_packages, setup
 
-
 # Utility Function to read the README file
 # Used for the long_description. It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
