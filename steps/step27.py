c7a36868f9fb8418df6487b5c8fad7a8dbe7be63
rain0430 <j2hoon85@naver.com>
2021-03-08 17:11:17 +0900

    [Add] new file updated

--- steps/step27.py
+++ steps/step27.py
@@ -10,41 +10,65 @@ from dezero.utils import plot_dot_graph
 
 class Sin(Function):
     def forward(self, x):
+        """
+        Forward of sin(x)
+        
+        returns: float
+        """
         y = np.sin(x)
-        return y 
+        return y
     
     def backward(self, gy):
+        """
+        differentiation of sin(x) is equal to cos(x)
+        gx : backward of sin(x), forward of sin(x) * derivation of sin(x)
+        
+        returns: float
+        """
         x = self.inputs[0].data
         gx = gy * np.cos(x)
-        return gx
-
+        return y
+        
 def sin(x):
     return Sin()(x)
 
+'''when x= 𝝿/4, differentiate y=sin(x).'''
 x = Variable(np.array(np.pi/4))
 y = sin(x)
 y.backward()
 
 print(y.data)
-print(x.grad)
+print(x.grad.data) # x의 미분 값
 
-def my_sin(x, threshold = 1e-150):
+import math
+
+def my_sin(x, threshold=0.0001):
+    
+    """ calculate talor series of sin(x) to 100000th derivation, unless (i)th derivation of sin(x) is smaller than threshold(0.0001)
+    Args:
+    threshold : absolut difference f(x) and f(x+𝛥)
+    
+    parameters :  
+    c : (i)th of function [(-1)ˆi/(2*i+1)!]
+    t : (i)th derivation of sin(x), multiply (i)th of function c and (i)th of xˆ(2*i+1)
+    y : tayor series value of sin(x).
+    
+    """
     y = 0
     for i in range(100000):
-        c = (-1) ** i / math.factorial(2 * i + 1)
-        t = c * x ** (2 * i + 1)
-        y = y + t
+        c = (-1)**i / math.factorial(2*i+1)
+        t = c * x **(2*i+1)
+        y = y+t
         if abs(t.data) < threshold:
             break
-    
     return y
 
-x = Variable(np.array(np.pi / 4))
+x = Variable(np.array(np.pi/4))
 y = my_sin(x)
 y.backward()
 
-print(y.data)
-print(x.grad)
+print(y.data) #my_sin 함수에 의한 값
+print(x.grad.data) # x의 미분 값
 
 x.name = 'x'
 y.name = 'y'
---
+++ steps/step28.py
@@ -0,0 +1,82 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import numpy as np
+from dezero import Variable
+# import dezero's simple_core explicitly
+import dezero
+if not dezero.is_simple_core:
+    from dezero.core_simple import Variable
+    from dezero.core_simple import setup_variable
+    setup_variable()
+
+
+def rosenbrock(x0, x1):
+    y = 100 * (x1 - x0 ** 2) ** 2 + (x0 - 1) ** 2
+    return y
+
+
+x0 = Variable(np.array(0.0))
+x1 = Variable(np.array(2.0))
+lr = 0.001
+iters = 1000
+
+for i in range(iters):
+    print(x0, x1)
+
+    y = rosenbrock(x0, x1)
+
+    x0.cleargrad()
+    x1.cleargrad()
+    y.backward()
+
+    x0.data -= lr * x0.grad
+    x1.data -= lr * x1.grad
+
+lr=0.001 #학습률
+iters=1000 #epoch
+result1 = []
+result2 = []
+for i in range(iters):
+    print(x0.data,x1.data)
+    y=rosenbrock(x0,x1)
+    x0.cleargrad()
+    x1.cleargrad()
+    y.backward()
+    x0.data=x0.data-lr*x0.grad
+    x1.data=x1.data-lr*x1.grad
+    result1.append(x0.data)
+    result2.append(x1.data)
+
+print(result1)
+print(result2)
+print(result1[-2:-1])
+print(result2[-2:-1])
+
+# =============================================================================
+# Visualize for computational graph
+# =============================================================================
+
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from matplotlib import cm
+
+x0 = np.arange(-2, 2, 0.15)
+x1 = np.arange(-1, 3, 0.15)
+x0, x1 = np.meshgrid(x0, x1)
+z = rosenbrock(x0,x1)
+
+figRos = plt.figure(figsize=(12, 7))
+axRos = figRos.gca(projection='3d')
+surf = axRos.plot_surface(x0, x1, z, cmap=cm.get_cmap(),linewidth=0, antialiased=False)
+axRos.set_zlim(0, 2000)
+figRos.colorbar(surf, shrink=0.5, aspect=10)
+# plt.show()
+
+plt.figure(figsize=(12, 7))
+plt.contour(x0,x1,z,700)
+for i,j in zip(result1,result2):
+    plt.plot(i,j,marker='o',markersize=5, color ='cyan')
+    plt.plot(1.0,1.0,marker='x',markersize=15, color ='r')
+
+plt.show()
